8cb822ce45ec967ea772831844d78048a7e81185
mbax <matt@phozop.net>
2012-09-21 14:49:39 -0400

    SQL change - drop ban_code. should be in separate table

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,12 +1,10 @@
 CREATE TABLE IF NOT EXISTS `SQLBans_bans` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ban_code` varchar(20) NOT NULL DEFAULT '',
   `username` varchar(16) NOT NULL,
   `reason` tinytext NOT NULL,
   `admin` varchar(16) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `banned` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQUE` (`ban_code`),
   KEY `username` (`username`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
